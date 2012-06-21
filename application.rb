3e502f0bc87bbdcac3df82c04188f28fc8e48932
Bryan Rite <bryan@bryanrite.com>
2012-06-21 15:23:34 -0700

    Create a game and kick off the outbound call.

--- application.rb
+++ application.rb
@@ -20,8 +20,16 @@ get '/' do
 end
 
 post '/start_game' do
-  puts params[:phone]
-  redirect '/hang_man/1000'
+  phone = clean_number params[:phone]
+  word = WORD_LIST.sample
+  game = Game.create(word: word, guesses: %w(), phone_number: phone)
+
+  logger.info "Starting a game with the word: #{word} to phone number: #{phone}"
+
+  # Make the outbound call and start the game.
+  HTTParty.get "http://api.tropo.com/1.0/sessions?action=create&token=#{settings.tropo_app_token}&game=#{game.id}"
+
+  redirect "/hang_man/#{game.id}"
 end
 
 get '/hang_man/:id' do
@@ -33,9 +41,15 @@ post '/start_game.json' do
   v = Tropo::Generator.parse request.env["rack.input"].read
   game = Game[v[:session][:parameters][:game]]
   session[:game_id] = game.id
+  session[:guesses] = 0
 
   t = Tropo::Generator.new
-  t.call(to: "+#{game.phone_number}")
+  t.call(to: "#{game.phone_number}")
   t.say(value: 'Starting game.')
   t.response
 end
+
+private
+  def clean_number(phone_number)
+    return "+#{phone_number.gsub(/\D/,'')}"
+  end
