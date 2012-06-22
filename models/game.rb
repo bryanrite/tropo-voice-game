7f56e00bf3a4785864ee5382ed6b5f256ddd225e
Bryan Rite <bryan@bryanrite.com>
2012-06-21 19:08:22 -0700

    Calculate win algorithm.

--- models/game.rb
+++ models/game.rb
@@ -3,14 +3,27 @@ class Game < Sequel::Model
   plugin :validation_helpers
   self.raise_on_typecast_failure = false
 
+  @@wins = [0b111_000_000,
+            0b000_111_000,
+            0b000_000_111,
+            0b100_100_100,
+            0b010_010_010,
+            0b001_001_001,
+            0b100_010_001,
+            0b001_010_100]
+
   def validate
     super
     validates_presence [:phone_number, :board]
     validates_type Array, [:board]
   end
 
-  def won?
-    false
+  def won? player
+    # bit-map based tic tac toe winner algorithm.
+    @@wins.any? do |win|
+      moves = board.inject(0) { |accum, position| (accum * 2) + ((position == player) ? 1 : 0) }
+      (win ^ moves) & win == 0
+    end
   end
 
   def over?
@@ -24,6 +37,12 @@ class Game < Sequel::Model
 
   def valid_move?(move)
     (1..9).include?(move) && board[move-1] =~ /\d/
+  end
+
+  def make_computers_choice
+    choice = board.select{ |x| x =~ /\d/}.sample.to_i
+    make_move choice, 'O'
+    choice
   end
 
 end
